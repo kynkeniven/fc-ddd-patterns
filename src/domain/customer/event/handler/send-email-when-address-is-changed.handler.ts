3893839dd00ecf4c5467c1e9518f8eb3ce90c553
Kynkeniven Grosskopf <kynkeniven.grosskopf@ntconsultcorp.com>
2023-08-09 10:58:22 -0300

    domain event desafio update

--- src/domain/customer/event/handler/send-email-when-address-is-changed.handler.ts
+++ src/domain/customer/event/handler/send-email-when-address-is-changed.handler.ts
@@ -1,10 +1,9 @@
 import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
 import AddressChangedEvent from "../address-changed.event";
-import CustomerCreatedEvent from "../customer-created.event";
 
 
 export default class SendEmailWhenAddressIsChangedHandler
-  implements EventHandlerInterface<CustomerCreatedEvent>
+  implements EventHandlerInterface<AddressChangedEvent>
 {
   handle(event: AddressChangedEvent): void {
     console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} alterado para: ${event.eventData.Address}`); 
